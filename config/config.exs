c14765425415f1ab4aa03112bd8a7e43182c2797
Kevin Edey <kevin.edey@gamgee.com>
2020-10-17 16:35:58 +0200

    Changed to port 443

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,5 @@
 use Mix.Config
 
 config :chuck, scheme: :http
-config :chuck, port: 80
+config :chuck, port: 443
 config :chuck, url: "https://api.chucknorris.io/jokes/"
---
+++ config/prod.exs
@@ -0,0 +1,1 @@
+config :chuck, port: 443
